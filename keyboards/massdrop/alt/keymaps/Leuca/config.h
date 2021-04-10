11375c501061950089519995d652d53a9523d290
Luca Magrone <luca.magrone@me.com>
2021-04-11 01:11:36 +0200

    first compiling draft

---
+++ keyboards/massdrop/alt/keymaps/Leuca/config.h
@@ -0,0 +1,1 @@
+#define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_CUSTOM_custom_indicators
---
+++ keyboards/massdrop/alt/keymaps/Leuca/ledmap.h
@@ -0,0 +1,34 @@
+static const uint8_t max_ledmap_layer = 1;
+static uint8_t ledmap[][DRIVER_LED_TOTAL][3] = {
+    [0] = {
+      {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF},
+      {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF},
+      {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF},            {RGB_OFF}, {RGB_OFF},
+      {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF},            {RGB_OFF}, {RGB_OFF},
+      {RGB_OFF}, {RGB_OFF}, {RGB_OFF},                                  {RGB_OFF},                                  {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF},
+      //Uderglow
+      {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF},
+      {RGB_OFF},                                                                                                                                                {RGB_OFF},
+      {RGB_OFF},                                                                                                                                                {RGB_OFF},
+      {RGB_OFF},                                                                                                                                                {RGB_OFF},
+      {RGB_OFF},                                                                                                                                                {RGB_OFF},
+      {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}
+    }
+    /*
+    [NULL] = {
+      {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF},
+      {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF},
+      {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF},            {RGB_OFF}, {RGB_OFF},
+      {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF},            {RGB_OFF}, {RGB_OFF},
+      {RGB_OFF}, {RGB_OFF}, {RGB_OFF},                                  {RGB_OFF},                                  {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF},
+      //Uderglow
+      {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF},
+      {RGB_OFF},                                                                                                                                                {RGB_OFF},
+      {RGB_OFF},                                                                                                                                                {RGB_OFF},
+      {RGB_OFF},                                                                                                                                                {RGB_OFF},
+      {RGB_OFF},                                                                                                                                                {RGB_OFF},
+      {RGB_OFF},                                                                                                                                                {RGB_OFF},
+      {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF},
+    }
+    */
+};
