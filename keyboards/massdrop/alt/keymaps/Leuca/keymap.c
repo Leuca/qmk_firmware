24abff569c99d63f2e59d1ad9ba463f267463129
Luca Magrone <luca.magrone@me.com>
2021-04-11 01:11:36 +0200

    first compiling draft

--- keyboards/massdrop/alt/keymaps/Leuca/keymap.c
+++ keyboards/massdrop/alt/keymaps/Leuca/keymap.c
@@ -1,6 +1,6 @@
 /* Copyright 2021 Leuca
  *
- * This program is free software: you can redistribute it and/or modify
+ * This program is free software: you can {RGB_OFF}istribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2 of the License, or
  * (at your option) any later version.
@@ -51,6 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     */
 };
+
 
 #define MODS_SHIFT  (get_mods() & MOD_BIT(KC_LSHIFT) || get_mods() & MOD_BIT(KC_RSHIFT))
 #define MODS_CTRL  (get_mods() & MOD_BIT(KC_LCTL) || get_mods() & MOD_BIT(KC_RCTRL))
