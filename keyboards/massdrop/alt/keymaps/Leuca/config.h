cbc31065feccac46fda74f8f2227ed05d5e75d49
Leuca <luca.magrone@me.com>
2021-04-19 19:18:15 +0200

    added rgb timeout

--- keyboards/massdrop/alt/keymaps/Leuca/config.h
+++ keyboards/massdrop/alt/keymaps/Leuca/config.h
@@ -1,2 +1,3 @@
 #define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_CUSTOM_custom_indicators
-#define F_SIDEGLOW
+//#define F_SIDEGLOW
+#define RGB_DISABLE_TIMEOUT 180000
--- keyboards/massdrop/alt/keymaps/Leuca/ledmap.h
+++ keyboards/massdrop/alt/keymaps/Leuca/ledmap.h
@@ -1,10 +1,11 @@
-#define RGB_ALP RGB_OFF
-#define RGB_MDF RGB_OFF
-#define RGB_NUM RGB_OFF
+#define RGB_ALP RGB_WHITE
+#define RGB_MDF RGB_CORAL
+#define RGB_NUM RGB_WHITE
 #define RGB_PLU RGB_GREEN
 #define RGB_MIN RGB_RED
+#define RGB_UGL 148, 38, 255
 
-
+#define MAX_SG 38
 
 static const uint8_t max_ledmap_layer = 2;
 static uint8_t ledmap[][DRIVER_LED_TOTAL][3] = {
@@ -12,45 +13,38 @@ static uint8_t ledmap[][DRIVER_LED_TOTAL][3] = {
       {RGB_MDF}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_ALP}, {RGB_ALP}, {RGB_MDF}, {RGB_MDF},
       {RGB_MDF}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_MDF},
       {RGB_MDF}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP},            {RGB_MDF}, {RGB_MDF},
-      {RGB_MDF}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP},            {RGB_MDF}, {RGB_MDF},
+      {RGB_MDF}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP},            {RGB_MDF}, {RGB_MDF}, {RGB_MDF},
       {RGB_MDF}, {RGB_MDF}, {RGB_MDF},                                  {RGB_ALP},                                  {RGB_MDF}, {RGB_MDF}, {RGB_MDF}, {RGB_MDF}, {RGB_MDF},
       //Uderglow (doesn't change anything if F_SIDEGLOW is defined)
-      {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF},
-      {RGB_OFF},                                                                                                                                                {RGB_OFF},
-      {RGB_OFF},                                                                                                                                                {RGB_OFF},
-      {RGB_OFF},                                                                                                                                                {RGB_OFF},
-      {RGB_OFF},                                                                                                                                                {RGB_OFF},
-      {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}
+      {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, //bottom strip
+      {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, // right side strip
+      {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, // top strip
+      {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL} // left side strip
     },
     [1] = {
       {RGB_MDF}, {RGB_BLUE}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_ALP}, {RGB_ALP}, {RGB_MDF}, {RGB_MDF},
       {RGB_MDF}, {RGB_ALP}, {RGB_PLU}, {RGB_PLU}, {RGB_PLU}, {RGB_PLU}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_MDF},
       {RGB_MDF}, {RGB_ALP}, {RGB_MIN}, {RGB_MIN}, {RGB_MIN}, {RGB_MIN}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP},            {RGB_MDF}, {RGB_PLU},
-      {RGB_MDF}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP},            {RGB_PLU}, {RGB_MIN},
+      {RGB_MDF}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP},            {RGB_MDF}, {RGB_PLU}, {RGB_MIN},
       {RGB_MDF}, {RGB_MDF}, {RGB_MDF},                                  {RGB_ALP},                                  {RGB_MDF}, {RGB_MDF}, {RGB_MDF}, {RGB_MIN}, {RGB_MDF},
       //Uderglow (doesn't change anything if F_SIDEGLOW is defined)
-      {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF},
-      {RGB_OFF},                                                                                                                                                {RGB_OFF},
-      {RGB_OFF},                                                                                                                                                {RGB_OFF},
-      {RGB_OFF},                                                                                                                                                {RGB_OFF},
-      {RGB_OFF},                                                                                                                                                {RGB_OFF},
-      {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}
+      {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, //bottom strip
+      {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, // right side strip
+      {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, // top strip
+      {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL} // left side strip
     }
     /*
     [NULL] = {
-      {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF},
-      {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF},
-      {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF},            {RGB_OFF}, {RGB_OFF},
-      {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF},            {RGB_OFF}, {RGB_OFF},
-      {RGB_OFF}, {RGB_OFF}, {RGB_OFF},                                  {RGB_OFF},                                  {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF},
-      //Uderglow
-      {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF},
-      {RGB_OFF},                                                                                                                                                {RGB_OFF},
-      {RGB_OFF},                                                                                                                                                {RGB_OFF},
-      {RGB_OFF},                                                                                                                                                {RGB_OFF},
-      {RGB_OFF},                                                                                                                                                {RGB_OFF},
-      {RGB_OFF},                                                                                                                                                {RGB_OFF},
-      {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF},
+      {RGB_MDF}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_ALP}, {RGB_ALP}, {RGB_MDF}, {RGB_MDF},
+      {RGB_MDF}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_MDF},
+      {RGB_MDF}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP},            {RGB_MDF}, {RGB_MDF},
+      {RGB_MDF}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP},            {RGB_MDF}, {RGB_MDF}, {RGB_MDF},
+      {RGB_MDF}, {RGB_MDF}, {RGB_MDF},                                  {RGB_ALP},                                  {RGB_MDF}, {RGB_MDF}, {RGB_MDF}, {RGB_MDF}, {RGB_MDF},
+      //Uderglow (doesn't change anything if F_SIDEGLOW is defined)
+      {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, //bottom strip
+      {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, // right side strip
+      {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, // top strip
+      {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL} // left side strip
     }
     */
 };
