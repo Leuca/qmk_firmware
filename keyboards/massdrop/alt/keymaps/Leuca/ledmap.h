a97b267efaf28411813edd44fab4a938dcd3b54e
Leuca <luca.magrone@me.com>
2021-04-19 13:26:21 +0200

    Fixed redefinition error

--- keyboards/massdrop/alt/keymaps/Leuca/ledmap.h
+++ keyboards/massdrop/alt/keymaps/Leuca/ledmap.h
@@ -1,5 +1,5 @@
 #define RGB_ALP RGB_OFF
-#define RGB_MOD RGB_OFF
+#define RGB_MDF RGB_OFF
 #define RGB_NUM RGB_OFF
 #define RGB_PLU RGB_GREEN
 #define RGB_MIN RGB_RED
@@ -9,11 +9,11 @@
 static const uint8_t max_ledmap_layer = 2;
 static uint8_t ledmap[][DRIVER_LED_TOTAL][3] = {
     [0] = {
-      {RGB_MOD}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_ALP}, {RGB_ALP}, {RGB_MOD}, {RGB_MOD},
-      {RGB_MOD}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_MOD},
-      {RGB_MOD}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP},            {RGB_MOD}, {RGB_MOD},
-      {RGB_MOD}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP},            {RGB_MOD}, {RGB_MOD},
-      {RGB_MOD}, {RGB_MOD}, {RGB_MOD},                                  {RGB_ALP},                                  {RGB_MOD}, {RGB_MOD}, {RGB_MOD}, {RGB_MOD}, {RGB_MOD},
+      {RGB_MDF}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_ALP}, {RGB_ALP}, {RGB_MDF}, {RGB_MDF},
+      {RGB_MDF}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_MDF},
+      {RGB_MDF}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP},            {RGB_MDF}, {RGB_MDF},
+      {RGB_MDF}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP},            {RGB_MDF}, {RGB_MDF},
+      {RGB_MDF}, {RGB_MDF}, {RGB_MDF},                                  {RGB_ALP},                                  {RGB_MDF}, {RGB_MDF}, {RGB_MDF}, {RGB_MDF}, {RGB_MDF},
       //Uderglow (doesn't change anything if F_SIDEGLOW is defined)
       {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF},
       {RGB_OFF},                                                                                                                                                {RGB_OFF},
@@ -23,11 +23,11 @@ static uint8_t ledmap[][DRIVER_LED_TOTAL][3] = {
       {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}
     },
     [1] = {
-      {RGB_MOD}, {RGB_BLUE}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_ALP}, {RGB_ALP}, {RGB_MOD}, {RGB_MOD},
-      {RGB_MOD}, {RGB_ALP}, {RGB_PLU}, {RGB_PLU}, {RGB_PLU}, {RGB_PLU}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_MOD},
-      {RGB_MOD}, {RGB_ALP}, {RGB_MIN}, {RGB_MIN}, {RGB_MIN}, {RGB_MIN}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP},            {RGB_MOD}, {RGB_PLU},
-      {RGB_MOD}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP},            {RGB_PLU}, {RGB_MIN},
-      {RGB_MOD}, {RGB_MOD}, {RGB_MOD},                                  {RGB_ALP},                                  {RGB_MOD}, {RGB_MOD}, {RGB_MOD}, {RGB_MIN}, {RGB_MOD},
+      {RGB_MDF}, {RGB_BLUE}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_ALP}, {RGB_ALP}, {RGB_MDF}, {RGB_MDF},
+      {RGB_MDF}, {RGB_ALP}, {RGB_PLU}, {RGB_PLU}, {RGB_PLU}, {RGB_PLU}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_MDF},
+      {RGB_MDF}, {RGB_ALP}, {RGB_MIN}, {RGB_MIN}, {RGB_MIN}, {RGB_MIN}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP},            {RGB_MDF}, {RGB_PLU},
+      {RGB_MDF}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP},            {RGB_PLU}, {RGB_MIN},
+      {RGB_MDF}, {RGB_MDF}, {RGB_MDF},                                  {RGB_ALP},                                  {RGB_MDF}, {RGB_MDF}, {RGB_MDF}, {RGB_MIN}, {RGB_MDF},
       //Uderglow (doesn't change anything if F_SIDEGLOW is defined)
       {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF}, {RGB_OFF},
       {RGB_OFF},                                                                                                                                                {RGB_OFF},
