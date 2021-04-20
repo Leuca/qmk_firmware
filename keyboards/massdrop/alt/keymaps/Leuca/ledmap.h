29813a4370527421001a0198b5c8ce3166e1b382
Luca Magrone <luca.magrone@me.com>
2021-04-20 14:40:33 +0200

    added hsv param compatibility for custom indicators effect

--- keyboards/massdrop/alt/keymaps/Leuca/ledmap.h
+++ keyboards/massdrop/alt/keymaps/Leuca/ledmap.h
@@ -1,50 +1,50 @@
-#define RGB_ALP RGB_WHITE
-#define RGB_MDF RGB_CORAL
-#define RGB_NUM RGB_WHITE
-#define RGB_PLU RGB_GREEN
-#define RGB_MIN RGB_RED
-#define RGB_UGL 148, 38, 255
+#define HSV_ALP HSV_WHITE
+#define HSV_MDF HSV_CORAL
+#define HSV_NUM HSV_WHITE
+#define HSV_PLU HSV_GREEN
+#define HSV_MIN HSV_RED
+#define HSV_UGL 148, 38, 255
 
 #define MAX_SG 38
 
 static const uint8_t max_ledmap_layer = 2;
 static uint8_t ledmap[][DRIVER_LED_TOTAL][3] = {
     [0] = {
-      {RGB_MDF}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_ALP}, {RGB_ALP}, {RGB_MDF}, {RGB_MDF},
-      {RGB_MDF}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_MDF},
-      {RGB_MDF}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP},            {RGB_MDF}, {RGB_MDF},
-      {RGB_MDF}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP},            {RGB_MDF}, {RGB_MDF}, {RGB_MDF},
-      {RGB_MDF}, {RGB_MDF}, {RGB_MDF},                                  {RGB_ALP},                                  {RGB_MDF}, {RGB_MDF}, {RGB_MDF}, {RGB_MDF}, {RGB_MDF},
+      {HSV_MDF}, {HSV_NUM}, {HSV_NUM}, {HSV_NUM}, {HSV_NUM}, {HSV_NUM}, {HSV_NUM}, {HSV_NUM}, {HSV_NUM}, {HSV_NUM}, {HSV_NUM}, {HSV_ALP}, {HSV_ALP}, {HSV_MDF}, {HSV_MDF},
+      {HSV_MDF}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_MDF},
+      {HSV_MDF}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP},            {HSV_MDF}, {HSV_MDF},
+      {HSV_MDF}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP},            {HSV_MDF}, {HSV_MDF}, {HSV_MDF},
+      {HSV_MDF}, {HSV_MDF}, {HSV_MDF},                                  {HSV_ALP},                                  {HSV_MDF}, {HSV_MDF}, {HSV_MDF}, {HSV_MDF}, {HSV_MDF},
       //Uderglow (doesn't change anything if F_SIDEGLOW is defined)
-      {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, //bottom strip
-      {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, // right side strip
-      {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, // top strip
-      {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL} // left side strip
+      {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, //bottom strip
+      {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, // right side strip
+      {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, // top strip
+      {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL} // left side strip
     },
     [1] = {
-      {RGB_MDF}, {RGB_BLUE}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_ALP}, {RGB_ALP}, {RGB_MDF}, {RGB_MDF},
-      {RGB_MDF}, {RGB_ALP}, {RGB_PLU}, {RGB_PLU}, {RGB_PLU}, {RGB_PLU}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_MDF},
-      {RGB_MDF}, {RGB_ALP}, {RGB_MIN}, {RGB_MIN}, {RGB_MIN}, {RGB_MIN}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP},            {RGB_MDF}, {RGB_PLU},
-      {RGB_MDF}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP},            {RGB_MDF}, {RGB_PLU}, {RGB_MIN},
-      {RGB_MDF}, {RGB_MDF}, {RGB_MDF},                                  {RGB_ALP},                                  {RGB_MDF}, {RGB_MDF}, {RGB_MDF}, {RGB_MIN}, {RGB_MDF},
+      {HSV_MDF}, {HSV_BLUE}, {HSV_NUM}, {HSV_NUM}, {HSV_NUM}, {HSV_NUM}, {HSV_NUM}, {HSV_NUM}, {HSV_NUM}, {HSV_NUM}, {HSV_NUM}, {HSV_ALP}, {HSV_ALP}, {HSV_MDF}, {HSV_MDF},
+      {HSV_MDF}, {HSV_MIN}, {HSV_PLU}, {HSV_PLU}, {HSV_PLU}, {HSV_PLU}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_MDF},
+      {HSV_MDF}, {HSV_MIN}, {HSV_MIN}, {HSV_PLU}, {HSV_MIN}, {HSV_MIN}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP},            {HSV_MDF}, {HSV_PLU},
+      {HSV_MDF}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP},            {HSV_MDF}, {HSV_PLU}, {HSV_MIN},
+      {HSV_MDF}, {HSV_MDF}, {HSV_MDF},                                  {HSV_ALP},                                  {HSV_MDF}, {HSV_MDF}, {HSV_MDF}, {HSV_MIN}, {HSV_MDF},
       //Uderglow (doesn't change anything if F_SIDEGLOW is defined)
-      {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, //bottom strip
-      {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, // right side strip
-      {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, // top strip
-      {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL} // left side strip
+      {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, //bottom strip
+      {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, // right side strip
+      {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, // top strip
+      {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL} // left side strip
     }
     /*
     [NULL] = {
-      {RGB_MDF}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_NUM}, {RGB_ALP}, {RGB_ALP}, {RGB_MDF}, {RGB_MDF},
-      {RGB_MDF}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_MDF},
-      {RGB_MDF}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP},            {RGB_MDF}, {RGB_MDF},
-      {RGB_MDF}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP}, {RGB_ALP},            {RGB_MDF}, {RGB_MDF}, {RGB_MDF},
-      {RGB_MDF}, {RGB_MDF}, {RGB_MDF},                                  {RGB_ALP},                                  {RGB_MDF}, {RGB_MDF}, {RGB_MDF}, {RGB_MDF}, {RGB_MDF},
+      {HSV_MDF}, {HSV_NUM}, {HSV_NUM}, {HSV_NUM}, {HSV_NUM}, {HSV_NUM}, {HSV_NUM}, {HSV_NUM}, {HSV_NUM}, {HSV_NUM}, {HSV_NUM}, {HSV_ALP}, {HSV_ALP}, {HSV_MDF}, {HSV_MDF},
+      {HSV_MDF}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_MDF},
+      {HSV_MDF}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP},            {HSV_MDF}, {HSV_MDF},
+      {HSV_MDF}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP}, {HSV_ALP},            {HSV_MDF}, {HSV_MDF}, {HSV_MDF},
+      {HSV_MDF}, {HSV_MDF}, {HSV_MDF},                                  {HSV_ALP},                                  {HSV_MDF}, {HSV_MDF}, {HSV_MDF}, {HSV_MDF}, {HSV_MDF},
       //Uderglow (doesn't change anything if F_SIDEGLOW is defined)
-      {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, //bottom strip
-      {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, // right side strip
-      {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, // top strip
-      {RGB_UGL}, {RGB_UGL}, {RGB_UGL}, {RGB_UGL} // left side strip
+      {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, //bottom strip
+      {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, // right side strip
+      {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, // top strip
+      {HSV_UGL}, {HSV_UGL}, {HSV_UGL}, {HSV_UGL} // left side strip
     }
     */
 };
